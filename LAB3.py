19c4b821a5ea6dd80c7a85f6eaf45e60920c9151
Tom Sagi <tom.sagy@gmail.com>
2020-08-10 15:51:16 +0300

    LAB 3 in Net4U course

---
+++ LAB3.py
@@ -0,0 +1,7 @@
+name = "Tom Sagi"
+mail = "tom.sagy@gmail.com"
+age =26
+
+print("your name: "+name+"\nyour email: "+mail+ "\nyour age: "+str(age))
+print("\nyour name backwards: "+name[::-1]+"\nyour age times 3: "+str(age*3))
+print(name in "idan ben dudu yuval shimon yael gal adam shahar yana")
