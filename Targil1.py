c6b090397827b147e6f4420054471d7936ae9b1a
Tom Sagi <tom.sagy@gmail.com>
2020-08-10 14:54:15 +0300

    First phyton excercise

---
+++ Targil1.py
@@ -0,0 +1,23 @@
+a = 12
+b= 11.56
+c = "Tom Sagi"
+
+#int
+print(a)
+print(type(a))
+
+#float
+print(b)
+print(type(b))
+
+#string
+print(c)
+print(type(c))
+
+#printing
+name = c
+age = 26
+print("\nHello " +name+"\nYour age is: "+str(age))
+
+mail = input("\nenter you email: ")
+print("your email is: "+mail)
